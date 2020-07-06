efee0ea938bec445f6f2315b9edc9b359e78e89d
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-07-06 18:49:33 +0900

    markdownの導入

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,48 @@
+module ApplicationHelper
+    require "redcarpet"
+    require "coderay"
+
+    class HTMLwithCoderay < Redcarpet::Render::HTML
+      def block_code(code, language)
+        language = language.sprit(':')[0] if language?
+
+        case language.to_s
+        when 'rb'
+          lang = :ruby
+        when 'yaml'
+          lang = :yaml
+        when 'css'
+          lamg = :css
+        when 'html'
+          lang = :html
+        when ''
+          lang = :md
+        else
+          lamg = language
+        end
+
+        CodeRay.scan(code, lamg).div
+      end
+    end
+
+    def markdown(text)
+      html_render = HTMLwithCoderay.new(
+        filter_html: true,
+        hard_wrap: true,
+        link_attributes: { rel: 'nofollow', target: "_blank"}
+      )
+      options = {
+        autolink: true,
+        space_after_headers: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        tables: true,
+        hard_wrap: true,
+        xhtml: true,
+        lax_html_blocks: true,
+        strikethrough: true
+      }
+      markdown = Redcarpet::Markdown.new(html_render, options)
+      markdown.render(text)
+    end
+end
